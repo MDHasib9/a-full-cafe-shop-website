f909c4f0f38ba9ea7771a89fa1ca6a0f84194437
Hasib <hasibnon@gmail.com>
2026-02-04 14:38:42 +0600

    fixing the nav bar position to fixed to the top

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-amber-900 text-white shadow-lg">
+    <nav className="bg-amber-900 top-0 right-0 fixed z-50 w-full text-white shadow-lg">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo and Brand */}
